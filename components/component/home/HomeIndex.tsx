ad3fcf7200909093fe10f522da030b5a46e7f72e
CaptainCoder <78704276+captainplanto@users.noreply.github.com>
2023-06-03 18:57:44 +0800

    build folder

--- components/component/home/HomeIndex.tsx
+++ components/component/home/HomeIndex.tsx
@@ -1,5 +1,4 @@
 import { useQuery, useReactiveVar } from "@apollo/client";
-import { Session } from "next-auth/core/types";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { Index } from ".";
@@ -8,9 +7,9 @@ import { invoiceMutation } from "../../../apollo/client/local/mutation";
 import { GET_ALL_INVOICE_BY_USER } from "../../../apollo/client/queries/invoice";
 import { SnackBarComponent } from "../../common/SnackBarComponent";
 import { LayOutPage } from "../../layout/pagelayout/LayOutPage.component";
-
+//ToDo: check why session here is thrwoing type error, id doesn't exisit on session.
 export const HomeIndex = () => {
-  const { data: session } = useSession();
+  const { data: session } = useSession() as any;
   const { invoices, showSnackBar } = useReactiveVar(invoiceVar);
   const { data } = useQuery(GET_ALL_INVOICE_BY_USER, {
     variables: { _id: session && session.id},
