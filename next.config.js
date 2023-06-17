63cd9408dd9d2c11fe9f46e2ad95144cddf8e670
CaptainCoder <78704276+captainplanto@users.noreply.github.com>
2023-06-18 00:15:25 +0800

    configuring base path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 // @type {import('next').NextConfig}
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
   },
-
-  assetPrefix: isProduction ? "https://demoinvoicing.vercel.app/" : "",
+  // basePath: isProduction ? "https://demoinvoicing.vercel.app/" : "",
+  // assetPrefix: isProduction ? "https://demoinvoicing.vercel.app/" : "",
 };
 
 module.exports = nextConfig;
