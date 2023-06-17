e413563295f7b113fdc5a5a2d87f3d60ff4bb290
CaptainCoder <78704276+captainplanto@users.noreply.github.com>
2023-06-18 00:15:25 +0800

    configuring base path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 // @type {import('next').NextConfig}
-
+const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
   },
+  basePath: isProduction ? "/your-new-url" : "",
+  assetPrefix: isProduction ? "/your-new-url/" : "",
 };
 
 module.exports = nextConfig;
